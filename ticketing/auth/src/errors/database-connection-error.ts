204238468b5dcd76cab7aa11cdfc5bccbbf67f3f
sam.02425 <saumilpatel2425@gmail.com>
2022-09-03 06:51:16 -0500

    custom error

--- ticketing/auth/src/errors/database-connection-error.ts
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -1,7 +1,20 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+    reason = 'Error connecting database';
+
     constructor() {
-        super();
+        super('Error connecting database');
         // Only because we are extending a built in class
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors(){
+        return [
+            {
+                message: this.reason
+            }
+        ];
     }
 }
--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
@@ -8,14 +7,15 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
     ) => {
-    if (err instanceof RequestValidationError) {
-        console.log('validation error');
-        };
-    if (err instanceof DatabaseConnectionError) {
-        console.log('Database error');
-    }
+        if (err instanceof CustomError) {
+            return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+        }
 
-    res.status(400).send({
-        message: err.message
-    });
-}
+        res.status(400).send({
+            errors: [
+                {
+                    message: 'something went wrong'
+                }
+            ]
+        });
+};
